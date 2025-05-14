496d88ef9af666a1a8b29e5ca8f4cdeeb5082aad
bostangeorgiana <161864553+bostangeorgiana@users.noreply.github.com>
2025-05-14 09:48:51 +0300

    login - page

--- backend/dto/userDTO.js
+++ backend/dto/userDTO.js
@@ -1,5 +1,7 @@
 const db = require("../dbConnection");
 const oracledb = require("oracledb");
+const bcrypt = require("bcrypt");
+
 
 // Data Transfer Object for User entity
 const userDTO = {
@@ -15,10 +17,10 @@ const userDTO = {
 
   async getAllUsers() {
     const result = await db.executeQuery("SELECT * FROM users", [], {
-      outFormat: oracledb.OUT_FORMAT_OBJECT,
+      outFormat: oracledb.OUT_FORMAT_OBJECT, //we don t want an array
     });
 
-    return result.rows.map(this.mapToUser);
+    return result.rows.map(this.mapToUser); //mapToUser -> face randul "curat"
   },
 
   async getUserById(id) {
@@ -41,8 +43,11 @@ const userDTO = {
     }
 
     //!!!!!!!!!!!!!!!!!!!!!!
-    const passwordHash = userData.password;
+    //const passwordHash = userData.password;
     //!!!!! NEEDS to be replace with proper hashing -or other method!!!!!
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(userData.password, saltRounds);
+
 
     const result = await db.executeQuery(
       `INSERT INTO users (username, email, password_hash) 
